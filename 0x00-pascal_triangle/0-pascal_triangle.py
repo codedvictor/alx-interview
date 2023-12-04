da9d124f1cfd27609519d5d574bc7cd9021e1547
codedvictor <victorkoladeige@gmail.com>
2023-12-04 11:25:02 +0100

    pascal triange

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+a function that returns a list of lists of integers
+representing the Pascal’s triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """
+    a list of lists of integers representing
+    the Pascal’s triangle of n
+    """
+
+    ptri = []
+
+    if n <= 0:
+        return ptri
+    for x in range(n):
+        xrow = []
+        for y in range(x + 1):
+            if (y == 0 or y == x):
+                xrow.append(1)
+            else:
+                xrow.append(ptri[x - 1][y - 1] + ptri[x - 1][y])
+        ptri.append(row)
+    return ptri
