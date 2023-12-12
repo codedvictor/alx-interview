d6fda5e7d619a4065c8c74b17c7ce0528638198d
codedvictor <victorkoladeige@gmail.com>
2023-12-12 13:33:32 +0100

    pycode update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
-'''The minimum operations coding challenge.
-'''
+"""The minimum operations coding challenge.
+"""
 
 
 def minOperations(n):
-    '''Computes the fewest number of operations needed to result
-    in exactly n H characters.
-    '''
+    """Computes the fewest number of operations needed.
+    """
     if not isinstance(n, int):
         return 0
-    ops_count = 0
-    clipboard = 0
+    oper_count = 0
+    clip = 0
     done = 1
-    # print('H', end='')
+
     while done < n:
-        if clipboard == 0:
-            # init (the first copy all and paste)
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-            # print('-(11)->{}'.format('H' * done), end='')
+        if clip == 0:
+            clip = done
+            done += clip
+            oper_count += 2
         elif n - done > 0 and (n - done) % done == 0:
-            # copy all and paste
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-            # print('-(11)->{}'.format('H' * done), end='')
-        elif clipboard > 0:
-            # paste
-            done += clipboard
-            ops_count += 1
-            # print('-(01)->{}'.format('H' * done), end='')
-    # print('')
-    return ops_count
+            clip = done
+            done += clip
+            oper_count += 2
+        elif clip > 0:
+            done += clip
+            oper_count += 1
+    return oper_count
